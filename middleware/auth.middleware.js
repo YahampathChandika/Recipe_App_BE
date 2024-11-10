2c6e009ef2db4d008291b47565c86cba61243336
Yahampath Chandika <yhmpth@gmail.com>
2024-11-10 22:02:41 +0530

    changes

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,15 +4,15 @@ const config = process.env;
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Extract the token from the Authorization header
+  const token = authHeader && authHeader.split(" ")[1]; 
 
   if (!token) {
     return res.status(403).json({ error: true, payload: "A token is required for authentication" });
   }
   try {
-    const decoded = jwt.verify(token, config.JWT_SECRET || "importantsecret"); // Using a config variable for the secret
+    const decoded = jwt.verify(token, config.JWT_SECRET || "importantsecret"); 
     req.user = decoded;
-    next(); // Call next() to proceed to the next middleware or route handler
+    next(); 
   } catch (err) {
     return res.status(401).json({ error: true, payload: "Invalid Token" });
   }
